34273c6db807a5e767628ba5420cc34f6d71d4b1
CODE <alhassansahih@gmail.com>
2025-07-20 01:49:03 +0100

    cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const transactionRoutes = require("./routes/transactionRoutes");
 const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
 
 // express app
 const app = express();
@@ -14,6 +15,12 @@ app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+app.use(
+  cors({
+    origin: "https://my-money-client.onrender.com",
+    credentials: true,
+  })
+);
 
 // routes
 app.use("/api/transactions", transactionRoutes);
