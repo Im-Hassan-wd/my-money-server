f02651b18e2213908d98f0b02999ce8adcc5fcea
CODE <alhassansahih@gmail.com>
2025-07-20 02:13:29 +0100

    testing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,10 @@ const router = express.Router();
 // login
 router.post("/login", loginUser);
 
+router.get("/login", (req, res) => {
+  res.json({ mssg: "GET login :)" });
+});
+
 // signup
 router.post("/signup", signupUser);
 
